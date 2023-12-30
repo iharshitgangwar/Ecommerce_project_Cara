2a4b052f46112e1c6fafa0ed8260532f161878e2
Harshit Gangwar <52398035+iharshitgangwar@users.noreply.github.com>
2023-12-30 18:47:35 +0530

    All Pages Updated

--- script/index.js
+++ script/index.js
@@ -11,3 +11,28 @@ if (closes){
         nav.classList.remove('active');
     })
 }
+
+let mainImg=document.getElementById('mainIng');
+const smallImg=document.getElementsByClassName('small-img');
+
+
+
+smallImg[1].onclick=function(){
+
+    mainImg.src=smallImg[1].src;
+}
+smallImg[0].onclick=function(){
+
+    mainImg.src=smallImg[0].src;
+}
+
+smallImg[2].onclick=function(){
+
+    mainImg.src=smallImg[2].src;
+}
+
+smallImg[3].onclick=function(){
+
+    mainImg.src=smallImg[3].src;
+}
+
